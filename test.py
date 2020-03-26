8677437b8aa8bc9b5d87991c718d6ca32048cdcb
EngeloperChen <1435535886@qq.com>
2020-03-26 09:09:38 +0800

    balabala

---
+++ test.py
@@ -0,0 +1,17 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+def say_hi(first_name, last_name):
+    print("=======>" + first_name + ' ' + last_name)
+    full_name = first_name + " " + last_name
+    print("Hi, " + full_name)
+    print("How old are you " + full_name + "?")
+
+
+
+say_hi("zoe", "chan")
+
+say_hi(first_name1, last_name1)
+
+
+
